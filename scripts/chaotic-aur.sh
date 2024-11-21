cc2682d1df8b858c87b7c24ac2e7317b00e2ba00
Chris Winters <chris.winters@coldcanuck.ca>
2024-11-20 21:03:24 -0500

    Update chaotic-aur.sh

--- scripts/chaotic-aur.sh
+++ scripts/chaotic-aur.sh
@@ -8,7 +8,7 @@ pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-# Append the Chaotic AUR repository to the end of pacman.conf
+# Append the Chaotic AUR repository to the end of /etc/pacman.conf
 {
     printf '\n'
     printf '# User Defined Repositories\n\n'
