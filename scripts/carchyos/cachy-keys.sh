6a8b6cc44ed3071e539f3ceeb2f86b38e2fe9f7f
Chris Winters <chris.winters@coldcanuck.ca>
2025-12-19 21:24:22 -0500

    Add CachyOS key download and signing commands
    Added commands to download and sign the CachyOS key.

--- scripts/carchyos/cachy-keys.sh
+++ scripts/carchyos/cachy-keys.sh
@@ -3,3 +3,5 @@
 # Download and sign the CachyOS key
 pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key F3B607488DB35A47
+
+# Download the CachyOS keyring and mirrorlist
